493a847840dd118600096e0c637050966f58a291
Alexander <sajget@ya.ru>
2020-04-26 01:06:24 +0300

    Basic awaiting on timer

--- src/test.cpp
+++ src/test.cpp
@@ -1,13 +1,21 @@
 #include "test.h"
 
 #include <iostream>
+#include <chrono>
 
 #include <boost/asio.hpp>
 
+using namespace std::chrono_literals;
+
 boost::asio::awaitable<void> foo() {
+    boost::asio::executor io = co_await boost::asio::this_coro::executor;
+
     std::cout << "Hello" << std::endl;
+
+    boost::asio::steady_timer timer(io, 1000ms);
+    co_await timer.async_wait(boost::asio::use_awaitable);
+
     std::cout << "World" << std::endl;
-    co_return;
 }
 
 void test() {
