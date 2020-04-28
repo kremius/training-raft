ead074b3748368a6992fc04956f5d52d667d0dd1
Alexander <sajget@ya.ru>
2020-04-28 22:58:30 +0300

    Very basic exception handling

--- src/listener.cpp
+++ src/listener.cpp
@@ -8,18 +8,21 @@ namespace traft
 using tcp = boost::asio::ip::tcp;
 
 asio::awaitable<void> listener() {
-    auto executor = co_await asio::this_coro::executor;
-    tcp::acceptor acceptor(executor, {tcp::v4(), 4321});
+    try {
+        auto executor = co_await asio::this_coro::executor;
+        tcp::acceptor acceptor(executor, {tcp::v4(), 4321});
 
-    fmt::print("Starting accept new connections...\n");
+        fmt::print("Starting accept new connections...\n");
 
-    while (true) {
-        // TODO: errors handling (i.e. port already in use)
-        tcp::socket socket = co_await acceptor.async_accept(asio::use_awaitable);
+        while (true) {
+            tcp::socket socket = co_await acceptor.async_accept(asio::use_awaitable);
 
-        fmt::print("New incoming connection: {}\n", socket.remote_endpoint().address().to_string());
+            fmt::print("New incoming connection: {}\n", socket.remote_endpoint().address().to_string());
 
-        socket.close();
+            socket.close();
+        }
+    } catch (const std::exception &ex) {
+        fmt::print("Error: {}\n", ex.what());
     }
 }
 
--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,15 @@
 #include "listener.h"
 
+#include <iostream>
+
+#include <fmt/core.h>
+
 int main(int /*argc*/, char */*argv*/[]) {
-    boost::asio::io_context io_context(1);
-    boost::asio::co_spawn(io_context, traft::listener, boost::asio::detached);
-    io_context.run();
+    try {
+        boost::asio::io_context io_context(1);
+        boost::asio::co_spawn(io_context, traft::listener, boost::asio::detached);
+        io_context.run();
+    } catch (const std::exception &ex) {
+        fmt::print("Error: {}\n", ex.what());
+    }
 }
