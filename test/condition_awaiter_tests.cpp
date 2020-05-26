09219005c7a10f521e89917f43c36f473ac68abf
Alexander <sajget@ya.ru>
2020-05-26 20:59:08 +0300

    More tests for ConditionAwaiter

--- test/condition_awaiter_tests.cpp
+++ test/condition_awaiter_tests.cpp
@@ -4,6 +4,26 @@
 
 #include "logging.h"
 
+TEST(ConditionAwaiter, Defaults) {
+    asio::io_context context(1);
+
+    traft::ConditionAwaiter<int> awaiter(&context);
+
+    bool finished = false;
+    auto waiter = [&awaiter, &finished]() mutable -> asio::awaitable<void> {
+        co_await awaiter.wait([](int v) { return v == 0; });
+        finished = true;
+    };
+
+    boost::asio::co_spawn(context, waiter, boost::asio::detached);
+
+    context.poll_one();
+    EXPECT_FALSE(finished);
+
+    context.poll_one();
+    EXPECT_TRUE(finished);
+}
+
 TEST(ConditionAwaiter, Basics) {
     asio::io_context context(1);
 
@@ -19,12 +39,50 @@ TEST(ConditionAwaiter, Basics) {
 
     context.poll_one();
     EXPECT_FALSE(finished);
+
     awaiter.updateData(4);
     EXPECT_FALSE(finished);
+
     context.poll_one();
     EXPECT_FALSE(finished);
+
     awaiter.updateData(5);
     EXPECT_FALSE(finished);
+
     context.poll_one();
     EXPECT_TRUE(finished);
+}
+
+TEST(ConditionAwaiter, MultipleWait) {
+    asio::io_context context(1);
+
+    traft::ConditionAwaiter<int> awaiter(&context);
+
+    bool finished1 = false;
+    bool finished2 = false;
+    auto waiter1 = [&awaiter, &finished1]() mutable -> asio::awaitable<void> {
+        co_await awaiter.wait([](int v) { return v > 3; });
+        finished1 = true;
+    };
+    auto waiter2 = [&awaiter, &finished2]() mutable -> asio::awaitable<void> {
+        co_await awaiter.wait([](int v) { return v > 4; });
+        finished2 = true;
+    };
+
+    boost::asio::co_spawn(context, waiter1, boost::asio::detached);
+    boost::asio::co_spawn(context, waiter2, boost::asio::detached);
+
+    context.poll();
+    EXPECT_FALSE(finished1);
+    EXPECT_FALSE(finished2);
+
+    awaiter.updateData(4);
+    context.poll();
+    EXPECT_TRUE(finished1);
+    EXPECT_FALSE(finished2);
+
+    awaiter.updateData(5);
+    context.poll();
+    EXPECT_TRUE(finished1);
+    EXPECT_TRUE(finished2);
 }
