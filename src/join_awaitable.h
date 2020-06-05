25dc9f8efe85dc69869d41c0c47e112040e71e09
Alexander <sajget@ya.ru>
2020-06-05 22:39:53 +0300

    Variadic awaitables support added

--- src/join_awaitable.h
+++ src/join_awaitable.h
@@ -6,55 +6,45 @@
 
 namespace traft {
 
-template<typename LeftType, typename RightType>
-asio::awaitable<std::variant<LeftType, RightType>> any(
-    asio::awaitable<LeftType> left,
-    asio::awaitable<RightType> right,
-    const asio::use_awaitable_t<>& token = asio::use_awaitable) {
-    auto executor = co_await asio::this_coro::executor;
+template<typename... AwaitableTypes>
+asio::awaitable<std::variant<AwaitableTypes...>> fastest(asio::awaitable<AwaitableTypes>... awaitables) {
     // TODO: executor should always be strand
+    auto executor = co_await asio::this_coro::executor;
 
-    using HandlerFunction = void(error_code, std::variant<LeftType, RightType>);
-    using HandlerType = typename asio::async_result<asio::use_awaitable_t<>, HandlerFunction>::handler_type;
+    // The lambda right below is called after the coroutine suspension.
+    // 'hanler' is the cororutine handler, so it should be called to resume coroutine.
+    auto implementation = [&executor, ...awaitables = std::move(awaitables)]<std::size_t... Indexes>(
+        auto handler, std::index_sequence<Indexes...>) mutable {
+        // `HandlerType` is movable-only, so store is mandatory here
+        auto handler_store = std::make_shared<std::optional<decltype(handler)>>();
+        handler_store->emplace(std::move(handler));
+        (boost::asio::co_spawn(
+            executor,
+            // TODO: check that 'executor' won't be removed too early
+            [&executor, awaitables = std::move(awaitables), handler_store]() mutable -> asio::awaitable<void> {
+                auto result = co_await std::move(awaitables);
+                if (!handler_store->has_value()) {
+                    // We're too late - other awaiter was faster.
+                    co_return;
+                }
+                asio::post(executor, [result = std::move(result), handler = std::move(handler_store->value())]() mutable {
+                    // TODO: exceptions?
+                    handler(
+                        errc::make_error_code(errc::success),
+                        std::variant<AwaitableTypes...>{std::in_place_index_t<Indexes>{}, std::move(result)});
+                });
+                handler_store->reset();
+            }, boost::asio::detached), ...);
+    };
 
-    auto handler_store = std::make_shared<std::optional<HandlerType>>();
+    auto proxy = [implementation = std::move(implementation)](auto handler) mutable {
+        implementation(std::move(handler), std::make_index_sequence<sizeof...(AwaitableTypes)>{});
+    };
 
     // Using built-in asio free function in order to properly implement async call
+    using HandlerFunction = void(error_code, std::variant<AwaitableTypes...>);
     co_return co_await async_initiate<const asio::use_awaitable_t<>, HandlerFunction>(
-        // The lambda right below is called after the coroutine suspension
-        [&executor, left = std::move(left), right = std::move(right), handler_store = std::move(handler_store)](auto handler) mutable {
-            handler_store->emplace(std::move(handler));
-            boost::asio::co_spawn(
-                executor,
-                // TODO: check that 'executor' and 'left' won't be removed too early
-                [&executor, left = std::move(left), handler_store]() mutable -> asio::awaitable<void> {
-                    auto result = co_await std::move(left);
-                    if (!handler_store->has_value()) {
-                        // We're too late - other awaiter was faster.
-                        co_return;
-                    }
-                    asio::post(executor, [result = std::move(result), handler = std::move(handler_store->value())]() mutable {
-                        // TODO: exceptions?
-                        handler(errc::make_error_code(errc::success), std::move(result));
-                    });
-                    handler_store->reset();
-                }, boost::asio::detached);
-            boost::asio::co_spawn(
-                executor,
-                // TODO: check that 'executor' and 'left' won't be removed too early
-                [&executor, right = std::move(right), handler_store]() mutable -> asio::awaitable<void> {
-                    auto result = co_await std::move(right);
-                    if (!handler_store->has_value()) {
-                        // We're too late - other awaiter was faster.
-                        co_return;
-                    }
-                    asio::post(executor, [result = std::move(result), handler = std::move(handler_store->value())]() mutable {
-                        // TODO: exceptions?
-                        handler(errc::make_error_code(errc::success), std::move(result));
-                    });
-                    handler_store->reset();
-                }, boost::asio::detached);
-        }, token);
+        std::move(proxy), asio::use_awaitable);
 }
 
 } // namespace traft
--- test/join_awaitable_tests.cpp
+++ test/join_awaitable_tests.cpp
@@ -8,11 +8,12 @@
 
 using namespace std::chrono_literals;
 
-TEST(AnyAwaitable, Basics) {
+TEST(FastestAwaitable, Basics) {
     asio::io_context context;
 
     struct Error {};
     struct Result {};
+    struct Result2 { int v = 0; };
 
     auto generate = [&](auto duration, auto value) -> asio::awaitable<decltype(value)> {
         boost::asio::steady_timer timer(context, duration);
@@ -23,13 +24,28 @@ TEST(AnyAwaitable, Basics) {
     // TODO: check that timings are correct - the test should take ~6ms, not more or less
     auto coroutine = [&]() -> asio::awaitable<void> {
         EXPECT_TRUE(std::holds_alternative<Error>(
-            co_await traft::any(generate(1ms, Error{}), generate(3ms, Result{}))));
+            co_await traft::fastest(generate(0ms, Error{}), generate(3ms, Result{}))));
         EXPECT_TRUE(std::holds_alternative<Error>(
-            co_await traft::any(generate(3ms, Result{}), generate(1ms, Error{}))));
+            co_await traft::fastest(generate(3ms, Result{}), generate(0ms, Error{}))));
         EXPECT_TRUE(std::holds_alternative<Result>(
-            co_await traft::any(generate(3ms, Error{}), generate(1ms, Result{}))));
+            co_await traft::fastest(generate(3ms, Error{}), generate(0ms, Result{}))));
         EXPECT_TRUE(std::holds_alternative<Result>(
-            co_await traft::any(generate(1ms, Result{}), generate(3ms, Error{}))));
+            co_await traft::fastest(generate(0ms, Result{}), generate(3ms, Error{}))));
+
+        EXPECT_TRUE(std::holds_alternative<Error>(
+            co_await traft::fastest(generate(1ms, Error{}), generate(2ms, Result{}), generate(3ms, Result2{}))));
+        EXPECT_TRUE(std::holds_alternative<Result>(
+            co_await traft::fastest(generate(2ms, Error{}), generate(1ms, Result{}), generate(3ms, Result2{}))));
+        EXPECT_TRUE(std::holds_alternative<Result2>(
+            co_await traft::fastest(generate(3ms, Error{}), generate(2ms, Result{}), generate(1ms, Result2{}))));
+
+        auto v1 = co_await traft::fastest(generate(0ms, Result2{2}), generate(1ms, Result2{1}));
+        // TODO: ASSERT_EQ(v1.index(), 0);
+        EXPECT_EQ(std::get<0>(v1).v, 2);
+
+        auto v2 = co_await traft::fastest(generate(10ms, Result2{2}), generate(0ms, Result2{1}));
+        // TODO: ASSERT_EQ(v1.index(), 0);
+        EXPECT_EQ(std::get<1>(v2).v, 1);
     };
 
     boost::asio::co_spawn(context, coroutine, boost::asio::detached);
