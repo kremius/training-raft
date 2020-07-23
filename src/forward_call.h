50b323386ad947cc848052d0bb4ef786709ae813
Alex <sajget@ya.ru>
2020-07-23 17:33:37 +0300

    Work guard for forward call

--- src/forward_call.h
+++ src/forward_call.h
@@ -13,14 +13,17 @@ asio::awaitable<std::invoke_result_t<CallbackType>> forward_call(
     asio::executor to_executor,
     CallbackType &&callback,
     const asio::use_awaitable_t<>& token = asio::use_awaitable) {
-    // TODO: work guard is mandatory https://www.boost.org/doc/libs/1_73_0/doc/html/boost_asio/reference/asynchronous_operations.html
     auto from_executor = co_await asio::this_coro::executor;
+    // Still somewhat unclear is guard really needed in coroutines, because
+    // this_coro::executor somehow exists, so maybe some other work guard is
+    // already created by co_spawn or something like that.
+    auto guard = asio::make_work_guard(from_executor);
     // Using built-in asio free function in order to properly implement async call
     co_return co_await async_initiate<const asio::use_awaitable_t<>, void (error_code, std::invoke_result_t<CallbackType>)>(
         // The lambda right below is called after the coroutine suspension
-        [from_executor, to_executor](auto handler, CallbackType &&callback) {
+        [from_executor, to_executor, guard = std::move(guard)](auto handler, CallbackType &&callback) {
             // Request execution of our 'callback' on the `to` executor
-            asio::post(to_executor, [handler = std::move(handler), from_executor, callback = std::move(callback)]() mutable {
+            asio::post(to_executor, [handler = std::move(handler), from_executor, callback = std::move(callback), guard = std::move(guard)]() mutable {
                 try {
                     // Work's done on the line below
                     auto result = callback();
@@ -31,8 +34,8 @@ asio::awaitable<std::invoke_result_t<CallbackType>> forward_call(
                         // called 'co_await forward_call(to, [](){ /* something */ })' in the first place
                         handler(errc::make_error_code(errc::success), std::move(result));
                     });
-                } catch (const boost::system::system_error &ex) {
-                    asio::post(from_executor, [handler = std::move(handler), code = ex.code()]() mutable {
+                } catch (const boost::system::system_error &error) {
+                    asio::post(from_executor, [handler = std::move(handler), code = error.code()]() mutable {
                         // The line below will resume the coroutine and throw exception
                         handler(code, std::invoke_result_t<CallbackType>{});
                     });
